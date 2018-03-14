d1c568c69817b4c39cb02f4f9f8429415fc1ef42
tee <turnyanszki@gmail.com>
2018-03-14 14:37:58 +0100

    declare Field

--- src/Field.java
+++ src/Field.java
@@ -1,2 +1,27 @@
 public class Field {
+    Thing thing;
+    Field[] neighbours= new Field[4];
+
+    public Field(){}
+
+    public void SetThing(Thing t)
+    {
+
+    }
+
+    public Field GetNeighbour(Directions d)
+    {
+
+    }
+
+    public Thing GetThing()
+    {
+
+    }
+
+    public void Operate()
+    {
+
+    }
+
 }
