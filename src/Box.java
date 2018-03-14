acd769f1add7fa506cba95698708edeb5ecd80bc
Levente Lenart <leviiibog@gmail.com>
2018-03-14 14:52:57 +0100

    Box: create class

---
+++ src/Box.java
@@ -0,0 +1,33 @@
+
+public class Box extends Thing {
+
+	Box(Field field) {
+		super(field);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void Destroy() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public boolean InteractWorker(Directions d) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean Movable(Directions d) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean InteractBox(Directions d) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
