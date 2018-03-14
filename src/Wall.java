8e39e09a9c76d15c774de8edb08724b07d493faa
Levente Lenart <leviiibog@gmail.com>
2018-03-14 14:51:17 +0100

    Wall: create class

---
+++ src/Wall.java
@@ -0,0 +1,29 @@
+
+public class Wall extends Thing {
+
+	Wall(Field field) {
+		super(field);
+	}
+
+	@Override
+	public void Destroy() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public boolean InteractWorker(Directions d) {
+		return false;
+	}
+
+	@Override
+	public boolean Movable(Directions d) {
+		return false;
+	}
+
+	@Override
+	public boolean InteractBox(Directions d) {
+		return false;
+	}
+
+}
