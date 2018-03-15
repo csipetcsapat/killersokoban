19f5c22a083ba0b451223c7253f94651fc87bcb2
Levente Lenart <leviiibog@gmail.com>
2018-03-15 17:14:10 +0100

    GameManager: create class

---
+++ src/GameManager.java
@@ -0,0 +1,12 @@
+
+public class GameManager {
+	private Worker currentPlayer;
+	
+	public void setCurrentPlayer(Worker player) {
+		currentPlayer = player;
+	}
+	
+	public Worker getCurrentPlayer() {
+		return currentPlayer;
+	}
+}
