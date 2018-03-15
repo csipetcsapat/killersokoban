063cbaa5263fddf2eb1ccc11da8d7c2b40fbeb47
Levente Lenart <leviiibog@gmail.com>
2018-03-15 17:11:11 +0100

    Worker: implement Move

--- src/Worker.java
+++ src/Worker.java
@@ -4,6 +4,22 @@ public class Worker extends Thing {
 	Worker(Field field) {
 		super(field);
 	}
+	
+	public boolean Move(Directions d) {
+		Field currentField = GetField();
+		Field nextField = currentField.GetNeighbour(d);
+		Thing t = nextField.GetThing();
+		
+		if (t == null || t.InteractWorker(d)) {
+			currentField.SetThing(null);
+			nextField.SetThing(this);
+			SetField(nextField);
+
+			return true;
+		}
+		
+		return false;
+	}
 
 	@Override
 	public void Destroy() {
