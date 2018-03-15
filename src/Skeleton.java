de61f454d15b47aaee7bf3da33550c9d5a77ed01
Levente Lenart <leviiibog@gmail.com>
2018-03-15 17:15:03 +0100

    Skeleton: implement a sample test case (PushBoxOnEmptyField)

--- src/Skeleton.java
+++ src/Skeleton.java
@@ -1,18 +1,20 @@
 
 public class Skeleton {
+	public static final GameManager gm = new GameManager();
+	public static final Logger log = new Logger();
 
-	public static void main(String[] args) {
-		Logger log = new Logger();
+	public static void main(String[] args) {		
+		Field f1 = new Field();
+		Field f2 = new Field();
+		Field f3 = new Field();
 		
-		log.call("Test", "t1", "test()");
-		log.call("Test", "t1", "test()");
-		log.call("Test", "t1", "test()");
-		log.call("Test", "t1", "test()");
+		f1.SetNeighbour(Directions.RIGHT, f2);
+		f2.SetNeighbour(Directions.RIGHT, f3);
 		
-		log.endCall();
-		log.endCall();
-		log.endCall();
-		log.endCall();
+		Worker w = new Worker(f1);
+		Box b = new Box(f2);
+		f2.SetThing(b);
+		w.Move(Directions.RIGHT);
 	}
 
 }
