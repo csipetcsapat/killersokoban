3318e0ca989ca8d0a676bb259cf48413c1120030
Levente Lenart <leviiibog@gmail.com>
2018-05-13 19:47:31 +0200

    Revert "Set MainWindow background"
    This reverts commit 5ac0c321dfc6e47e1f919353c98755184ed99386.

--- src/graphics/MainWindow.java
+++ src/graphics/MainWindow.java
@@ -1,12 +1,6 @@
 package graphics;
 
-import java.io.File;
-import java.io.IOException;
-
-import javax.imageio.ImageIO;
-import javax.swing.ImageIcon;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
 
 public class MainWindow {
 	private JFrame frame;
@@ -15,12 +9,6 @@ public class MainWindow {
 		frame = new JFrame("Killer Sokoban");
 		frame.setSize(width, height);
 		frame.setLayout(null);
-		try {
-			frame.setContentPane(new JLabel(new ImageIcon(ImageIO.read(new File("res/menu.png")))));
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
 	}
 	
 	public void Show() {
